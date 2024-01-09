a4aeca56b4bfd32429c38ffe52152426d688817b
Kantawit64 <6452500198@stu.pim.ac.th>
2024-01-09 13:03:30 +0700

    Add test

---
+++ test1_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/volk/test1_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/volk/test_ws/devel;/home/volk/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/volk/test_ws/devel;/home/volk/catkin_ws/devel;/opt/ros/noetic' != '' else []
---
+++ test1_ws/build/volk/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/volk/test1_ws/src/volk
+# Build directory: /home/volk/test1_ws/build/volk
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
